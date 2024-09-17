1167a974d4ecce48734a430621652e9681e882c4
BlackHeartNet <juniormarketingfc@gmail.com>
2024-09-16 22:29:14 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,16 +1,12 @@
-// Adicione validações e lógica de login aqui
-const form = document.querySelector('form');
+// scripts.js
 
-form.addEventListener('submit', (event) => {
-  event.preventDefault(); // Previne o envio padrão do formulário
+// Adiciona efeito 3D suave ao mover o mouse sobre a caixa de login
+document.querySelector('.login-box').addEventListener('mousemove', function (e) {
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+  this.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
 
-  // Obtenha os valores do usuário e senha
-  const username = document.getElementById('username').value;
-  const password = document.getElementById('password').value;
-
-  // Faça a validação e o login aqui
-  // ...
-
-  // Se o login for bem-sucedido, redirecione para outra página
-  // window.location.href = '/dashboard';
+document.querySelector('.login-box').addEventListener('mouseleave', function () {
+  this.style.transform = 'rotateY(0deg) rotateX(0deg)';
 });
